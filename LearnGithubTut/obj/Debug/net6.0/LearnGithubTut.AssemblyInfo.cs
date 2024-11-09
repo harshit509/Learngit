79e2b7bdc219ce48446f5f3fa9ffc59aebbf4b88
harshit <82759198+harshit509@users.noreply.github.com>
2024-11-09 17:24:39 +0530

    change view

--- LearnGithubTut/obj/Debug/net6.0/LearnGithubTut.AssemblyInfo.cs
+++ LearnGithubTut/obj/Debug/net6.0/LearnGithubTut.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnGithubTut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25867549ba6511941c6da19c07ebdd8f8a9950b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnGithubTut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnGithubTut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
